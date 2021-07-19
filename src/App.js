7da99baf66afa4a97d08b689eb93a8a69d0b4fd0
ashwinashok98 <ashwinashok98@gmail.com>
2021-07-19 18:59:42 +0530

    Addes scss package, ,created homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
-
+import Homepage from './homepage.component';
 class App extends Component{
   constructor(){
     super();
   }
   render(){
     return(
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Hello
-      </a>
-    </header>
+    <div>
+      <Homepage/>    
   </div>);
   };
 } 
