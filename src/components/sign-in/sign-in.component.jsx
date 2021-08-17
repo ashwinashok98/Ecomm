c5a0968d14ff70c668f3eef142ef280bb31c9004
ashwinashok98 <ashwinashok98@gmail.com>
2021-08-17 08:43:00 +0530

    finishing signin component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle } from "../../firebase/firebase.utils";
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +11,17 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const {email,password}= this.state;
+    try{  
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({ email: "", password: "" });
+    }
+    catch(error){
+      console.error(error)
+    }
+    
   };
   handleChange = (event) => {
     const { name, value } = event.target;
