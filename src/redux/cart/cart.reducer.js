5d1309e8a323bdf8e1dff659712e442002430385
ashwinashok98 <ashwinashok98@gmail.com>
2021-09-10 12:59:58 +0530

    added arrow to increase,decrease quantity in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeITemFromCart } from "./cart.utils";
 
 
 const INITIAL_STATE = {
@@ -18,6 +18,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+      case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeITemFromCart(state.cartItems, action.payload)
+      };
+      case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem=>cartItem.id !== action.payload.id)
       };
 
     default:
