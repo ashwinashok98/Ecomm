718b644bbf3816a0025621a9f594d79f56d0ae04
ashwinashok98 <ashwinashok98@gmail.com>
2021-10-04 14:57:39 +0530

    updated shop reducer and built collection componen

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-
+import { PersistGate } from "redux-persist/integration/react";
+import  { store,persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
+    </PersistGate>
+      
     </BrowserRouter>
    
   </Provider>,document.getElementById('root')
