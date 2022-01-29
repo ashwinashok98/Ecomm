6a57e3519c8de94430ba4b3ee01362ae5989c59e
ashwinashok98 <ashwinashok98@gmail.com>
2022-01-29 18:14:52 +0530

    modified store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,7 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
-if(process.env.NODE_ENV==='development'){
-    middlewares.push(logger)
-}
+const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store); 
