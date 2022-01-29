dd09d974877a0f7dadaa48041e1c190fa3c4177a
ashwinashok98 <ashwinashok98@gmail.com>
2022-01-29 18:42:22 +0530

    cleaned store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
+
 if(process.env.NODE_ENV==='development'){
     middlewares.push(logger)
 }
